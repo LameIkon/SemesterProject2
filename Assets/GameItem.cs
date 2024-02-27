24d284ec8750fc00c15b5c13403b6c8c0c566230
LameIkon <troelsjelsbak@hotmail.com>
2024-02-27 21:50:36 +0100

    Deleted Inventory, moved scripts

--- Assets/GameItem.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameItem : ScriptableObject
-{
-    public string Name;
-    public int Value; 
-}
--- Assets/InventoryFiller.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class InventoryFiller : ScriptableObject
-{
-    public abstract GameItem[] ItemFiller();
-
-}
