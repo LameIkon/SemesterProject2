58d13a7e582a0ea53ac1950b2ae7b7c8854a4219
chaplinchap <jonas.linnemann@live.dk>
2024-03-08 12:15:44 +0100

    Update ToolbarManager.cs

--- Assets/_Scripts/Scriptables/Inventory/ToolbarManager.cs
+++ Assets/_Scripts/Scriptables/Inventory/ToolbarManager.cs
@@ -13,14 +13,7 @@ public class ToolbarManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {      
-        if(Input.inputString != null)
-        {
-            bool isNumber = int.TryParse(Input.inputString, out int pressedNumber);
-            if(isNumber && pressedNumber >= 1 && pressedNumber <= 6 ) 
-            {
-                ChangeSelectedSlot(pressedNumber -1);
-            }
-        }
+
 
         //if (Input.GetKeyUp(KeyCode.Alpha1))
         //{
@@ -35,6 +28,34 @@ public class ToolbarManager : MonoBehaviour
         //        }
         //    }         
         //}              
+    }
+
+    public void SelectSlot()
+    {      
+        if (Input.inputString != null)
+        {
+            bool isNumber = int.TryParse(Input.inputString, out int pressedNumber);
+            if (isNumber && pressedNumber >= 1 && pressedNumber <= 6)
+            {
+                ChangeSelectedSlot(pressedNumber - 1);
+                
+            }
+        }      
+    }
+
+    public void TriggerSlot (int slot)
+    {
+        var itemInSlot = _toolbarInventory.GetSlots[slot];
+        slot = _selectedSlot;
+        if (itemInSlot.ItemObject != null) //checks that there is an item object in the slot
+        {
+            itemInSlot.ItemObject.Action(); //calls the action function on that object
+            itemInSlot.AddAmount(-1); //substract 1 from the amount
+            if (itemInSlot._Amount <= 0) //Checks if the amount of the item is 0
+            {
+                itemInSlot.RemoveItem(); //removes item, so that we can't use it infinitely
+            }
+        }
     }
 
     public void ChangeSelectedSlot(int slot)
