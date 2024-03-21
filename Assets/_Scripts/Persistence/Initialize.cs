6bd4ebc9111306e2f6a2ba4eff37bac21aabf039
JohanPedersen11 <Johan.munch.2002@gmail.com>
2024-03-21 20:17:24 +0100

    small adjustment to initilze script
    made it more robust and ensures only 1 instances. It can now be called in other scripts also now, to not limit developers to be required to start in MainMenu

--- Assets/_Scripts/Persistence/Initialize.cs
+++ Assets/_Scripts/Persistence/Initialize.cs
@@ -7,7 +7,9 @@ public class Initialize : MonoBehaviour
     public static Initialize instance;
 
     private GameObject _persistanceObject; // Store the value. used to activate and disable the GameObject
-    private bool _initialized = true;
+    private static bool _initialized = true; // Ensures only 1 instance of running some code once
+    private bool _checkInstances; // Used to ensure that OnLevelWasLoaded only get run after it gets checked if there is more instances.
+
     private void Awake()
     {
         if (instance != null)
@@ -18,36 +20,57 @@ public class Initialize : MonoBehaviour
         {
             instance = this;
         }
-        DontDestroyOnLoad(gameObject); // This script should never be destroyed
-    }
 
-    private void OnLevelWasLoaded() //Called whenever a new scene is loaded
-    {
-        if (_initialized)
+        DontDestroyOnLoad(gameObject); // This script should never be destroyed
+
+        if (_initialized) // Just does this once
         {
             _persistanceObject = Instantiate(Resources.Load("PersistObjects")) as GameObject; // Instantiate the persistant Objects
             DontDestroyOnLoad(_persistanceObject); // Ensure not being destroyed 
             _persistanceObject.SetActive(false); // Initialize it to be false to not effect anything in MainMenu
-            _initialized = false;
-        }
+            _initialized = false; // This will never be able to run again
 
-        switch (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("MainMenu")) // if MainMenu
+            if (SceneManager.GetActiveScene() != SceneManager.GetSceneByName("MainMenu")) // If we start the game in another scene besides the MainMenu
+            {
+                Debug.Log("Awake");
+                LoadGame();
+            }
+
+            _checkInstances = true; // Turns to true. This should give the script time to ensure only 1 instance
+        }
+    }
+
+    private void OnLevelWasLoaded() //Called whenever a new scene is loaded
+    {
+        if (_checkInstances) // Retrict running before _checkInstances bool is checked.
         {
-            case true: // Disable persistanceObjects when in MainMenu
-                Debug.Log("Main Menu Loaded");
-                //LanternDisabler._LoadedSTATIC = false; // Something Jonas has
-                _persistanceObject.SetActive(false); 
-
-                break;
-            default: // Activate persistanceObjects when loaded game
-                Debug.Log("Game loaded");
-                _persistanceObject.SetActive(true);
-                if (!LanternDisabler._LanternSTATIC)
-                {
-                    LanternDisabler._LoadedSTATIC = true; // Something Jonas has
-                }
-                break;
+            Debug.Log("load level");
+            switch (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("MainMenu")) // if MainMenu
+            {
+                case true: // Disable persistanceObjects when in MainMenu
+                    LoadMenu();
+                    break;
+                default: // Activate persistanceObjects when loaded game
+                    LoadGame();
+                    break;
+            }
         }
+    }
+
+    void LoadGame()
+    {
+        Debug.Log("Game loaded");
+        _persistanceObject.SetActive(true);
+        if (!LanternDisabler._LanternSTATIC)
+        {
+            LanternDisabler._LoadedSTATIC = true; // Something Jonas has
+        }
+    }
+
+    void LoadMenu()
+    {
+        Debug.Log("Main Menu Loaded");
+        _persistanceObject.SetActive(false);
     }
 
 
