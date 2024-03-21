be22897532bf23182d51dd5204e00da5729ee928
chaplinchap <jonas.linnemann@live.dk>
2024-03-21 22:30:13 +0100

    Made Adam items + Start of fuel burning

--- Assets/_Scripts/Interactables/Bonfire.cs
+++ Assets/_Scripts/Interactables/Bonfire.cs
@@ -1,3 +1,4 @@
+using Ink.Parsed;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,22 +8,119 @@ public class Bonfire : MonoBehaviour
 {
     [SerializeField] private FloatVariable _systemFloat;
     [SerializeField] private float _restoreValue;
-   
+
+    [SerializeField] private static GameObject _bonfireCanvas;
+    [SerializeField] private InventoryObject _bonfireInventory;
+    private StaticInterface _bonfireInterface;
+    [SerializeField] private bool _canOpenBonfire = false;
+    [SerializeField] private int _burningTime;
+
+
+
+    private bool _bonfireLit = false;
+
+    private IEnumerator _burnCoroutine;
+    private IEnumerator _leftoverCoroutine;
+
+    private bool _turn = false;
+
+
+
+    /// <summary>
+    /// Can't put wood in AND get warmth without going in and out
+    /// </summary>
+
+    private void Update()
+    {
+        if (LanternDisabler._LoadedSTATIC)
+        {
+            _bonfireCanvas = GameObject.FindWithTag("Bonfire");          
+            _bonfireCanvas.SetActive(false);
+
+            _bonfireInterface = _bonfireCanvas.GetComponent<StaticInterface>();
+            _bonfireInterface._Inventory = _bonfireInventory;
+        }
+    }
+
+    void OnEnable()
+    {
+        InputReader.OnInteractEvent += HandleInteract;
+        InputReader.OnPickEvent += HandleInteract;
+    }
+
+    private void OnDisable()
+    {
+        InputReader.OnInteractEvent -= HandleInteract;
+        InputReader.OnPickEvent -= HandleInteract;
+    }
+
+    void HandleInteract()
+    {
+        if (_canOpenBonfire)
+        {
+            OpenBonfire();
+        }
+    }
+
+    private void OpenBonfire()
+    {
+        _turn = !_turn;
+        _bonfireCanvas.SetActive(_turn);
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if (collision.CompareTag("Player"))
-        {                
-            _systemFloat.ApplyChange(_restoreValue);
+        {
+            _burnCoroutine = BurningTime(_burningTime);
+            _canOpenBonfire = true;
+            BurningWood();
+           // _systemFloat.ApplyChange(_restoreValue);
         }       
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-        {    
-            _systemFloat.ApplyChange(-_restoreValue);
+        {
+            _canOpenBonfire = false;            
+            _leftoverCoroutine = LeftoverTime(_burningTime);
+
+            if (_bonfireLit)
+            {
+                StopCoroutine(_burnCoroutine);
+                _systemFloat.ApplyChange(-_restoreValue);
+                StartCoroutine(_leftoverCoroutine);
+            }            
         }
     }
+
+
+    private void BurningWood ()
+    {
+        if (_bonfireInventory.GetSlots[0].ItemObject._ItemType == ItemType.Fuel)
+        {
+            _bonfireLit = true;
+            StartCoroutine(_burnCoroutine);
+        }
+    }
+
+
+    IEnumerator BurningTime (int time)
+    {
+        _systemFloat.ApplyChange(_restoreValue);
+        yield return new WaitForSeconds(time);
+        _bonfireInventory.GetSlots[0].RemoveItem();
+        _systemFloat.ApplyChange(-_restoreValue);
+        _bonfireLit = false;
+    }
+
+    IEnumerator LeftoverTime (int time)
+    {
+        yield return new WaitForSeconds(time);
+        _bonfireInventory.GetSlots[0].RemoveItem();
+    }
+
 }
---
+++ Assets/_Scripts/Scriptables/Inventory/Items/Liquor.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Item Object", menuName = "Iventory System/Items/AlchoholItem")]
+
+public class Liquor : ItemObject
+{
+    [SerializeField] private FloatVariable _SystemFloatHunger;
+    [SerializeField] private FloatVariable _SystemFloatFreeze;
+
+    public int RestoreHunger;
+    public int RestoreFreeze;
+
+    public void Awake()
+    {
+        _ItemType = ItemType.Food;
+    }
+
+    public override void Action()
+    {
+        _SystemFloatHunger.ApplyChange(RestoreHunger);
+        _SystemFloatHunger.ApplyChange(RestoreFreeze);
+
+    }
+
+    public override void DisableAction()
+    {
+
+    }
+}
