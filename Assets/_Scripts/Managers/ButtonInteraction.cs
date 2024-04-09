4e7ca8bb779027fcb808e44e5be4745fdb121094
JohanPedersen11 <Johan.munch.2002@gmail.com>
2024-04-09 03:06:19 +0200

    wip  buttons icons

--- Assets/_Scripts/Managers/ButtonInteraction.cs
+++ Assets/_Scripts/Managers/ButtonInteraction.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -11,6 +12,10 @@ public class ButtonInteraction : MonoBehaviour
     public Button[] buttons; // needs correct lineup
     public int currentIndex = -1;
     private bool startedSelection;
+
+    // Used to fade image in
+    private float fadeInTime = 0.1f;
+    private float currentAlpha = 0f;
 
     private void Start()
     {
@@ -82,6 +87,7 @@ public class ButtonInteraction : MonoBehaviour
 
     void SelectButton(int direction)
     {
+
         // Deselect the currently selected button
         buttons[currentIndex].Select();
 
@@ -90,6 +96,9 @@ public class ButtonInteraction : MonoBehaviour
 
         // Select the the new button.
         buttons[currentIndex].Select();
+
+        // Highlight Icons
+        //StartCoroutine(SelectionPolish());
     }
 
     void ActivateSelectedButton()
@@ -99,6 +108,60 @@ public class ButtonInteraction : MonoBehaviour
         {
             buttons[currentIndex].onClick.Invoke();
         }
-        
+    }
+
+    IEnumerator SelectionPolish()
+    {
+
+        // Check if the selected button has image children
+        Image[] childImages = buttons[currentIndex].GetComponentsInChildren<Image>();
+        Debug.Log(currentIndex);
+        // Skip the first image
+        for (int i = 1; i < childImages.Length; i++)
+        {
+            Image childImage = childImages[i];
+            while (currentAlpha < 1f)
+            {
+                // Calculate the new alpha value
+                currentAlpha += Time.deltaTime / fadeInTime;
+                currentAlpha = Mathf.Clamp01(currentAlpha);
+
+                // Apply the new alpha value to the image
+                Color imageColor = childImage.color;
+                imageColor.a = currentAlpha;
+                childImage.color = imageColor;
+
+                yield return null;
+            }
+        }
+    }
+
+    IEnumerator DeSelectionPolish()
+    {
+
+        // Check if the selected button has image children
+        Image[] childImages = buttons[currentIndex-1].GetComponentsInChildren<Image>();
+        Debug.Log(currentIndex);
+
+        currentAlpha = 1f;
+
+        // Skip the first image
+        for (int i = 1; i < childImages.Length; i++)
+        {
+            Image childImage = childImages[i];
+            while (currentAlpha > 0f)
+            {
+                // Calculate the new alpha value
+                currentAlpha -= Time.deltaTime / fadeInTime;
+                currentAlpha = Mathf.Clamp01(currentAlpha);
+
+                // Apply the new alpha value to the image
+                Color imageColor = childImage.color;
+                imageColor.a = currentAlpha;
+                childImage.color = imageColor;
+
+                yield return null;
+            }
+        }
     }
 }
