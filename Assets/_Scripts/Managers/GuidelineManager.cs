a66dbd43db08c42a98f82a04109bbf4d64990899
JohanPedersen11 <Johan.munch.2002@gmail.com>
2024-04-12 12:07:15 +0200

    tutorial WIP

--- Assets/_Scripts/Managers/GuidelineManager.cs
+++ Assets/_Scripts/Managers/GuidelineManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GuidelineManager : MonoBehaviour
 {
+    public static GuidelineManager instance { get; private set; }
+
+    [Header("Boolean Checkmark")]
     [SerializeField] private bool _showMovement;
     [SerializeField] private bool _showRunning;
     [SerializeField] private bool _showInventoryInteraction;
@@ -16,21 +19,81 @@ public class GuidelineManager : MonoBehaviour
     [SerializeField] private GameObject CampfireCanvas;
     [SerializeField] private GameObject CampfireContainer;
 
+    private void Awake()
+    {
+          // Ensure only 1 singleton of this script
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+
+
+        // Ensure all booleans are set to false on start
+        _showMovement = false;
+        _showRunning = false;
+        _showInventoryInteraction = false;
+        _showChestInteraction = false;
+        _showCampfireInteraction = false;
+        _showInventoryInteraction = false;
+
+    }
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { // test
+        _showMovement = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_showMovement)
+        {
+
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        InputReader.OnMoveEvent += StopMovement;
     }
 
     void ShowMovement()
     {
         MovementCanvas.SetActive(true);
+        _showMovement = true; // Checks that the movement guide has been shown
+    }
+
+    void StopMovement(Vector2 vector)
+    {
+        if (_showMovement)
+        {
+            _showMovement = false;
+            StartCoroutine(FadeOut(MovementCanvas));
+        }
+    }
+
+    void ShowRunning()
+    {
+        RunningCanvas.SetActive(true);
+    }
+
+    IEnumerator FadeIn(GameObject canvas)
+    {
+        yield return null;
+    }
+
+    IEnumerator FadeOut(GameObject canvas)
+    {
+        yield return null;
     }
 
 }
+
+
+
