1e894a83ce5012d3ec788aadf00819c13139f6ff
JohanPedersen11 <Johan.munch.2002@gmail.com>
2024-04-17 01:20:00 +0200

    item description almost done
    its working as it is but i found an oversight that item positions gets changed, so i need to make something that will look at the item name and then take the coresponding text

--- Assets/_Scripts/Interactables/ItemDescription.cs
+++ Assets/_Scripts/Interactables/ItemDescription.cs
@@ -1,58 +1,58 @@
-using Ink.Parsed;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class ItemDescription : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-
-
-    [Header("Data on this GameObject")]
-    [SerializeField] private Image _slot; // Slot image to check
+    
     [SerializeField, TextArea(2, 4)] private string _description; // The items description
+    [SerializeField, Tooltip("Set images you want to ignore")] private Sprite[] _ignoreImages; // Ignore if its a image you dont want shown
 
-    [Header("Description Canvas")]
-    [SerializeField] private GameObject _itemDescriptionCanvas; // The description Canvas
-    //[SerializeField] private Sprite _previousImage; // Used to compare with the previous
-    [SerializeField] private Sprite[] _ignoreImages; // Ignore if its a image you dont want shown
+    private Image _slot; // Slot image to check
+    private GameObject _itemDescriptionCanvas; // The description Canvas
+    
 
-    private bool _hovering;
 
     private void Awake()
     {
-        _hovering = false;
+        _itemDescriptionCanvas = GameObject.Find("ItemDescriptionHolder");
         _itemDescriptionCanvas.SetActive(false);
-
     }
 
     public void OnPointerEnter(PointerEventData eventData) // When mouse is hovering over it
     {
         _slot = GetComponent<Image>();
 
-        foreach (Sprite notIgnoredImage in _ignoreImages)
+        foreach (Sprite IgnoreImage in _ignoreImages)
         {
             // drumstick is equl to null
-            if (_slot.sprite != notIgnoredImage) // If the image is not ignored then show description
+            if (_slot.sprite == IgnoreImage) // If the image is not ignored then show description
+            {
+                break;
+            }
+            else
             {
                 ShowDescription();
+                CancelInvoke("HideDescription"); // Stop the script from trying to close it when we want it to be open
                 Debug.Log("true");
+                break;
             }
         }
-
-
     }
 
     public void OnPointerExit(PointerEventData eventData) // When mouse exit hovering over it
     {
-        _hovering = false;
         Invoke("HideDescription", 0.3f);
     }
 
     private void ShowDescription()
     {
+        _itemDescriptionCanvas.GetComponentInChildren<TextMeshProUGUI>().text = _description; // Insert the description from the GameObject
+
+        Image replaceImage = _itemDescriptionCanvas.transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<Image>(); // Get the specific image from the ItemDescriptionCanvas
+        replaceImage.sprite = _slot.sprite;
+
         _itemDescriptionCanvas.SetActive(true);
     }
 
@@ -60,5 +60,4 @@ public class ItemDescription : MonoBehaviour, IPointerEnterHandler, IPointerExit
     {
         _itemDescriptionCanvas.SetActive(false);
     }
-
 }
