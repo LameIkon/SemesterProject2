b23c79ef886570f9918f64391559fe36124712cf
4sela <adawa21@student.sdu.dk>
2024-04-26 01:00:30 +0200

    Foundation to Visuals

--- Assets/_Scripts/Visuals.cs
+++ Assets/_Scripts/Visuals.cs
@@ -4,39 +4,60 @@ using UnityEngine;
 
 public class Visuals : MonoBehaviour
 {
-    // Objects
-    private GameObject _volume;
+    [Header("Objects")] // -- Objects --
+    [SerializeField] private GameObject _volume;
+    [Space(5f)]
 
-    // Freeze
-    [Header("Freeze")]
-    [SerializeField] private FloatVariable _currentTemperature;
+    [Header("Health")] // -- Health Variables --
+    [SerializeField] private FloatReference _currentHealth;
+    [Space(5f)]
+    
+    [Header("Freeze")] // -- Freeze Variables --
+    [SerializeField] private FloatReference _currentTemperature;
     [SerializeField] private float _temperatureThreshold = 20;
-    private bool _isCold;
+    [SerializeField] private bool _isCold; //
+    [Space(5f)]
+
+    [Header("Hunger")] // -- Hunger Variables --
+    [SerializeField] private FloatReference _currentHunger;
+    //[Space(5f)]
     
     
-    void Awake()
+    // [Header("Heat")] // -- Heat Variables --
+    // [SerializeField] private FloatReference _current
+    // [Space(5f)]
+    
+    #region Unity Methods
+    private void Awake()
     {
-        // _volume = 
+        // _volume = GameObject.FindWithTag("Volume").GetComponent<Volume>();
     }
     
-    void Update()
+    private void Update()
     {
-        if (_currentTemperature < _temperatureThreshold)
-        {
-            EnableFreezeVisual();
-        }
-        else
-        {
-            DisableFreezeVisual();
+        switch (_currentTemperature < _temperatureThreshold) {
+            case true: EnableFreezeVisual();
+                break;
+            case false: DisableFreezeVisual();
+                break;
+
         }
     }
-    void EnableFreezeVisual()
+    #endregion
+    
+    #region Health Methods
+
+    #endregion
+    
+    #region Freeze Methods
+    private void EnableFreezeVisual()
     {
         _isCold = true;
     }
 
-    void DisableFreezeVisual()
+    private void DisableFreezeVisual()
     {
         _isCold = false;
     }
+    #endregion
 }
