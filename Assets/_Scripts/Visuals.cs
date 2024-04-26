8bd8b6034cdd747d29b8a47aadf6391dae481e3e
4sela <adawa21@student.sdu.dk>
2024-04-26 21:54:27 +0200

    + Freeze Effect

--- Assets/_Scripts/Visuals.cs
+++ Assets/_Scripts/Visuals.cs
@@ -1,63 +1,188 @@
-using System.Collections;
-using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
 
 public class Visuals : MonoBehaviour
 {
-    [Header("Objects")] // -- Objects --
-    [SerializeField] private GameObject _volume;
-    [Space(5f)]
-
-    [Header("Health")] // -- Health Variables --
-    [SerializeField] private FloatReference _currentHealth;
+    [Header("Objects")] 
+    [SerializeField] private Volume _volume;
     [Space(5f)]
     
-    [Header("Freeze")] // -- Freeze Variables --
+    [Header("Freeze")]
     [SerializeField] private FloatReference _currentTemperature;
-    [SerializeField] private float _temperatureThreshold = 20;
-    [SerializeField] private bool _isCold; //
+    [SerializeField] private float _threshold;
+    [SerializeField] private float _changeRate;
+    [SerializeField] private float _temperatureVolumeAboveThreshold;
+    [SerializeField] private float _temperatureVolumeBelowThreshold;
+    [SerializeField] protected bool _isCold;
+    [CanBeNull] private ClampedFloatParameter _cfp;
+    private float _defaultTVAboveT;
+    private float _defaultTVBelowT;
+    private bool _defaulter;
+    private WhiteBalance _whiteBalance;
+    private Vignette _vignette;
     [Space(5f)]
 
-    [Header("Hunger")] // -- Hunger Variables --
+    [Header("Health")] 
+    [SerializeField] private FloatReference _currentHealth;
+    [SerializeField] private float _healthThreshold;
+    [Space(5f)]
+    
+    [Header("Hunger")] 
     [SerializeField] private FloatReference _currentHunger;
-    //[Space(5f)]
-    
-    
-    // [Header("Heat")] // -- Heat Variables --
-    // [SerializeField] private FloatReference _current
-    // [Space(5f)]
+    [SerializeField] private float _hungerThreshold;
     
     #region Unity Methods
-    private void Awake()
-    {
-        // _volume = GameObject.FindWithTag("Volume").GetComponent<Volume>();
-    }
     
-    private void Update()
-    {
-        switch (_currentTemperature < _temperatureThreshold) {
-            case true: EnableFreezeVisual();
-                break;
-            case false: DisableFreezeVisual();
-                break;
+        private void Awake()
+        {
+            #region Freeze Variables
+            
+                _defaulter = true; // Assures that 
 
+                _defaultTVAboveT = _temperatureVolumeAboveThreshold; // For resetting _temperatureVolumeAboveThreshold
+                _defaultTVBelowT = _temperatureVolumeBelowThreshold; // For resetting _temperatureVolumeBelowThreshold
+                
+                _cfp = new ClampedFloatParameter(_defaultTVAboveT, _defaultTVBelowT, _defaultTVAboveT, true);
+                
+                WhiteBalance wb; 
+                Vignette vig;
+
+                switch (_volume.profile.TryGet<WhiteBalance>(out wb)) { case true: _whiteBalance = wb; break; }
+                switch (_volume.profile.TryGet<Vignette>(out vig)) { case true: _vignette = vig; break; }
+                
+            #endregion
         }
-    }
-    #endregion
-    
-    #region Health Methods
 
+        private void Update()
+        {
+            // print(_currentTemperature.Value);
+            _whiteBalance.temperature.SetValue(_cfp);
+            
+            switch (_currentTemperature < _threshold) {
+                case true: EnableFreezeVisual(); break;
+                case false: DisableFreezeVisual(); break;
+            }
+        }
+        
     #endregion
     
     #region Freeze Methods
-    private void EnableFreezeVisual()
+    
+        protected virtual void EnableFreezeVisual()
+        {
+            _defaulter = false;
+            _isCold = true;
+            
+            _temperatureVolumeBelowThreshold = _defaultTVBelowT; 
+            _cfp = new ClampedFloatParameter(_temperatureVolumeAboveThreshold, _defaultTVBelowT, _defaultTVAboveT, true);
+            
+            switch (_temperatureVolumeAboveThreshold > _defaultTVBelowT)
+            {
+                case true: 
+                    _temperatureVolumeAboveThreshold -= _changeRate; 
+                    break;
+            }
+        }
+
+        protected virtual void DisableFreezeVisual()
+        {
+            _isCold = false;
+            
+            if (_defaulter)
+            {
+                _cfp = new ClampedFloatParameter(_temperatureVolumeAboveThreshold, _defaultTVBelowT, _defaultTVAboveT, true);
+                return;
+            }
+            
+            _temperatureVolumeAboveThreshold = _defaultTVAboveT;
+            _cfp = new ClampedFloatParameter(_temperatureVolumeBelowThreshold, _defaultTVBelowT, _defaultTVAboveT, true);
+            
+            switch (_temperatureVolumeBelowThreshold < _defaultTVAboveT) 
+            {
+                case true: 
+                    _temperatureVolumeBelowThreshold += _changeRate; 
+                    break;
+            }
+        }
+        
+    #endregion
+    
+    #region Health Methods
+    
+        private void EnableLowHealthVisual()
+        {
+        
+        }
+    
+        private void DisableLowHealthVisual()
+        {
+        
+        }
+        
+    #endregion
+    
+    #region Hunger Methods
+        private void EnableHungerVisual()
+        {
+        
+        }
+
+        private void DisableHungerVisual()
+        {
+        
+        }
+        
+    #endregion
+}
+
+public sealed class HeatAbsorption : Visuals
+{
+    // [Header("Heat")] 
+    [SerializeField] private FloatReference _currentTemperature;
+    
+    
+    #region Unity Methods
+    
+        private void Awake()
+        {
+        
+        }
+    
+        private void Update()
+        {
+            switch (_isCold)
+            {
+                case true: this.EnableFreezeVisual(); break;
+                case false: this.DisableFreezeVisual(); break;
+            }   
+        }
+        
+    #endregion
+    
+    #region Heat Absorption Methods
+    
+        private void EnableAbsorptionVisual()
+        {
+            
+        }
+    
+        private void DisableAbsorptionVisual()
+        {
+        
+        }
+    
+    #endregion
+    
+    protected override void EnableFreezeVisual()
     {
-        _isCold = true;
+        
     }
 
-    private void DisableFreezeVisual()
-    {
-        _isCold = false;
+    protected override void DisableFreezeVisual() {
+        
     }
-    #endregion
 }
