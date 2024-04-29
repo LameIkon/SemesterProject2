bee0a217e941ae8cf13e8f9f89488cd47632e81b
4sela <adawa21@student.sdu.dk>
2024-04-29 10:36:17 +0200

    + Visual.cs upgrades MEN ikke færdigt

--- Assets/_Scripts/Visuals.cs
+++ Assets/_Scripts/Visuals.cs
@@ -1,4 +1,4 @@
-using JetBrains.Annotations;
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -12,72 +12,120 @@ public class Visuals : MonoBehaviour
     [SerializeField] private Volume _volume;
     [Space(5f)]
 
+    // ------------------------------------------------------------------------------------------------ \\
+    
     [Header("Health")] 
     [SerializeField] private FloatReference _currentHealth;
-    [SerializeField] private float _healthThreshold;
+    [SerializeField] private float _thresholdHealth;
+    [SerializeField] private float _changeRateHealth;
+    [SerializeField] private float _maximumIntensityRange;
+    [SerializeField] private float _minimumIntensityRange;
+    [SerializeField] private float _currentIntensity;
+    [SerializeField] private bool _isActive;
+    private ClampedFloatParameter _cfpHealth;
     private Vignette _vignette;
     [Space(5f)]
+    
+    // ------------------------------------------------------------------------------------------------ \\
     
     [Header("Temperature")]
     [SerializeField] private FloatReference _currentTemperature;
     [SerializeField] private Color _colorAboveThreshold; 
     [SerializeField] private Color _colorBelowThreshold;
-    [SerializeField] private float _temperatureThreshold;
-    [SerializeField] private float _changeRate;
-    [SerializeField] private float _temperatureVolumeAboveThreshold;
-    [SerializeField] private float _temperatureVolumeBelowThreshold;
+    [SerializeField] private float _thresholdTemperature;
+    [SerializeField] private float _changeRateTemperature;
+    [SerializeField] private float _volumeAboveThresholdTemperature;
+    [SerializeField] private float _volumeBelowThresholdTemperature;
     [SerializeField] protected bool _isCold;
     private float _defaultTVAboveT;
     private float _defaultTVBelowT;
     private bool _defaulter;
     private Image _temperatureIcon;
-    private ClampedFloatParameter _cfp;
+    private ClampedFloatParameter _cfpTemperature;
     private WhiteBalance _whiteBalance;
     [Space(5f)]
+    
+    // ------------------------------------------------------------------------------------------------ \\
     
     [Header("Hunger")] 
     [SerializeField] private FloatReference _currentHunger;
     [SerializeField] private float _hungerThreshold;
+    
+    // ------------------------------------------------------------------------------------------------ \\
     
     #region Unity Methods
     
         private void Awake()
         {
             #region Health Variables
-            
-                Vignette vig;   // Used to initialize Volume components
-            
-                switch (_volume.profile.TryGet<Vignette>(out vig)) { case true: _vignette = vig; break; }   // Initializes Volume components
+
+                _currentIntensity = _minimumIntensityRange;                                                                                          // Sets the current range to the minimum
+                _cfpHealth = new ClampedFloatParameter(_currentIntensity, _minimumIntensityRange, _maximumIntensityRange, _isActive);   // Sets the Volume Vignette to the current 
+                Vignette vig;                                                                                                                        // Used to initialize Volume components
+                switch (_volume.profile.TryGet<Vignette>(out vig)) { case true: _vignette = vig; break; }                                            // Initializes Volume components
             
             #endregion
             
             #region Temperature Variables
 
-                _temperatureIcon = GameObject.FindWithTag("TemperatureImage").GetComponent<Image>();
-            
-                _defaulter = true;  // Assures only one use of the if-statement in DisableFreezeVisual() 
-
-                _defaultTVAboveT = _temperatureVolumeAboveThreshold; // To reset _temperatureVolumeAboveThreshold after being changed
-                _defaultTVBelowT = _temperatureVolumeBelowThreshold; // To reset _temperatureVolumeBelowThreshold after being changed
-                
-                _cfp = new ClampedFloatParameter(_defaultTVAboveT, _defaultTVBelowT, _defaultTVAboveT, true);   // Sets the Volume Temperature to the default 
-                
-                WhiteBalance wb;    // Used to initialize Volume components
-
-                switch (_volume.profile.TryGet<WhiteBalance>(out wb)) { case true: _whiteBalance = wb; break; } // Initializes Volume components
+                _temperatureIcon = GameObject.FindWithTag("TemperatureImage").GetComponent<Image>();                                               // Initializes the temperature icon
+                _defaulter = true;                                                                                                                 // Assures only one use of the if-statement in DisableFreezeVisual() 
+                _defaultTVAboveT = _volumeAboveThresholdTemperature;                                                                               // To reset _temperatureVolumeAboveThreshold after being changed
+                _defaultTVBelowT = _volumeBelowThresholdTemperature;                                                                               // To reset _temperatureVolumeBelowThreshold after being changed
+                _cfpTemperature = new ClampedFloatParameter(_defaultTVAboveT, _defaultTVBelowT, _defaultTVAboveT, true);   // Sets the Volume Temperature to the default 
+                WhiteBalance wb;                                                                                                                   // Used to initialize Volume components
+                switch (_volume.profile.TryGet<WhiteBalance>(out wb)) { case true: _whiteBalance = wb; break; }                                    // Initializes Volume components
 
             #endregion
         }
 
         private void Update()
         {
+            #region Health Variables
+
+                _vignette.intensity.SetValue(_cfpHealth);            // Avoids NullReferences
+                switch (_currentHealth < _thresholdHealth)       
+                {
+                    case true: EnableLowHealthVisual(); break;
+                    case false: DisableLowHealthVisual(); break;
+                }
+
+            #endregion
             
-            _whiteBalance.temperature.SetValue(_cfp); // Avoids a NullReference
+            #region Temperature Variables
             
-            switch (_currentTemperature < _temperatureThreshold) {
-                case true: EnableFreezeVisual(); break;
-                case false: DisableFreezeVisual(); break;
+                _whiteBalance.temperature.SetValue(_cfpTemperature);     // Avoids NullReferences
+                switch (_currentTemperature < _thresholdTemperature) 
+                { 
+                    case true: EnableFreezeVisual(); break;
+                    case false: DisableFreezeVisual(); break;
+                }
+                
+            #endregion
+        }
+        
+    #endregion
+    
+    #region Health Methods
+    
+        private void EnableLowHealthVisual()
+        {
+            _isActive = true;
+            bool aboveThreshold = false;
+            
+            if (_currentIntensity > _maximumIntensityRange) { aboveThreshold = true; } // Omskrives på en anden måde, evt lave en metoder der gør boolen til true
+            if (_currentIntensity < _minimumIntensityRange) { aboveThreshold = false; }
+
+            switch (aboveThreshold)
+            {
+                case true: _currentIntensity -= _changeRateHealth; print("true"); break; 
+                case false: _currentIntensity += _changeRateHealth; print("false"); break;
             }
+        }
+    
+        private void DisableLowHealthVisual()
+        {
+            _isActive = false;
         }
         
     #endregion
@@ -91,14 +139,12 @@ public class Visuals : MonoBehaviour
             _defaulter = false; // Assures only one use of the if-statement in DisableFreezeVisual()
             _isCold = true;
             
-            _temperatureVolumeBelowThreshold = _defaultTVBelowT; 
-            _cfp = new ClampedFloatParameter(_temperatureVolumeAboveThreshold, _defaultTVBelowT, _defaultTVAboveT, true);
+            _volumeBelowThresholdTemperature = _defaultTVBelowT; 
+            _cfpTemperature = new ClampedFloatParameter(_volumeAboveThresholdTemperature, _defaultTVBelowT, _defaultTVAboveT, true);
             
-            switch (_temperatureVolumeAboveThreshold > _defaultTVBelowT)
+            switch (_volumeAboveThresholdTemperature > _defaultTVBelowT)
             {
-                case true: 
-                    _temperatureVolumeAboveThreshold -= _changeRate; 
-                    break;
+                case true: _volumeAboveThresholdTemperature -= _changeRateTemperature * Time.deltaTime; break;
             }
         }
 
@@ -106,39 +152,24 @@ public class Visuals : MonoBehaviour
         {
             _temperatureIcon.color = _colorAboveThreshold;
             _isCold = false;
-            
-            if (_defaulter) // Will only be accessed at the start of the game
+
+            switch (_defaulter) // Will only be accessed at the start of the game
             {
-                _cfp = new ClampedFloatParameter(_temperatureVolumeAboveThreshold, _defaultTVBelowT, _defaultTVAboveT, true);
-                return;
+                case true: _cfpTemperature = new ClampedFloatParameter(_volumeAboveThresholdTemperature, _defaultTVBelowT, _defaultTVAboveT, true); 
+                    return;
             }
             
-            _temperatureVolumeAboveThreshold = _defaultTVAboveT;
-            _cfp = new ClampedFloatParameter(_temperatureVolumeBelowThreshold, _defaultTVBelowT, _defaultTVAboveT, true);
+            _volumeAboveThresholdTemperature = _defaultTVAboveT;
+            _cfpTemperature = new ClampedFloatParameter(_volumeBelowThresholdTemperature, _defaultTVBelowT, _defaultTVAboveT, true);
             
-            switch (_temperatureVolumeBelowThreshold < _defaultTVAboveT) 
+            switch (_volumeBelowThresholdTemperature < _defaultTVAboveT) 
             {
-                case true: 
-                    _temperatureVolumeBelowThreshold += _changeRate; 
-                    break;
+                case true: _volumeBelowThresholdTemperature += _changeRateTemperature * Time.deltaTime; break;
             }
         }
         
     #endregion
     
-    #region Health Methods
-    
-        private void EnableLowHealthVisual()
-        {
-        
-        }
-    
-        private void DisableLowHealthVisual()
-        {
-        
-        }
-        
-    #endregion
     
     #region Hunger Methods
     
