d040620d11adb9e55a1c694c94ff882961bb19eb
4sela <adawa21@student.sdu.dk>
2024-05-06 12:07:08 +0200

    + added objectshake script

--- Assets/_Scripts/ObjectShake.cs
+++ Assets/_Scripts/ObjectShake.cs
@@ -1,28 +1,43 @@
-using System;
+using System.Collections;
+using System.Threading;
+using Cinemachine;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class ObjectShake : MonoBehaviour
 {
-    public float _ShakeAmount;
-    public bool _CanShake;
+    private RectTransform _rectTransform;
+    private Vector2 _originalPosition;
+    private float _speed = 1.0f;
+    private float _amount = 10.0f;
     
+    // public float _ShakeAmount;
+    // public bool _CanShake;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _originalPosition = _rectTransform.anchoredPosition;
+    }
+
     private void Update()
     {
-        if (_CanShake)
-        {
-            Shake();
-        }
+        Shake(_rectTransform, _originalPosition, _speed, _amount);
     }
 
-    public void Shake()
+    private void Shake(RectTransform rectTransform, Vector2 originalPosition, float speed, float amount)
+    { 
+        float offsetX = Mathf.Sin(Time.time * speed) * amount;
+        rectTransform.anchoredPosition = originalPosition + new Vector2(offsetX, 0f);
+    }
+
+    private void Shaker()
     {
-        Vector3 newPos = Random.insideUnitSphere * (Time.deltaTime * _ShakeAmount);
-        var position = transform.position;
-        newPos.y = position.y;
-        newPos.z = position.z;
-
-        transform.position = newPos;
+        // Vector3 newPos = Random.insideUnitSphere * (Time.deltaTime * _ShakeAmount);
+        // newPos.y = _rectTransform.transform.position.y;
+        // newPos.z = _rectTransform.transform.position.x;
+        //
+        // _rectTransform.transform.position = newPos;
     }
+    
 }
